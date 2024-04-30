4fffb1e45a6207b4a5b9e09a3d4b1c7f639f69e9
Melissa Castañon <161492866+MelissaCasta@users.noreply.github.com>
2024-04-30 01:13:39 -0600

    Update mainn.js

--- mainn.js
+++ mainn.js
@@ -117,8 +117,18 @@ document.addEventListener("DOMContentLoaded", function () {
                     if (ultimo.comando.includes("enciende el ventilador")) {
                         document.getElementById('ventilador').src = 'VP.gif';
                         console.log("Ventilador Encendido.");
-                    } else if (ultimo.comando.includes("apagar el ventilador")) {
-                        document.getElementById('ventilador').src = 'VA.gif';
+                    } else 
+                        if (ultimo.comando.includes("apagar el ventilador")) {
+                        document.getElementById('ventilador').src = 'VA.png';
+                        console.log("Ventilador Apagado.");
+                    }
+
+                    if (ultimo.comando.includes("enciende el ventilador")) {
+                        document.getElementById('ventilador').src = 'VP.gif';
+                        console.log("Ventilador Encendido.");
+                    } else 
+                        if (ultimo.comando.includes("apaga el ventilador")) {
+                        document.getElementById('ventilador').src = 'VA.pngf';
                         console.log("Ventilador Apagado.");
                     }
 
